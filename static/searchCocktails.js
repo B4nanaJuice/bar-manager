0d9249df5cba875cfa01f516ddb80433aef6821d
B4nanaJuice <57623972+B4nanaJuice@users.noreply.github.com>
2025-09-20 12:47:17 +0200

    Change the url and reload the page

--- static/searchCocktails.js
+++ static/searchCocktails.js
@@ -4,6 +4,13 @@ function searchCocktails() {
     ingredientList = tagsSpan.map(span => span.innerText);
 
     // Take the cocktail type
+    cocktailTypeSelect = document.querySelector(".cocktail_type");
+    cocktailType = cocktailTypeSelect.value || "";
 
     // Send the request and refresh the page
+    var url = new URL(window.location);
+    if (cocktailType != "") { url.searchParams.set("cocktail_type", cocktailType); }
+    if (ingredientList.length > 0) { url.searchParams.set("ingredient_preferences", ingredientList.join(",")); }
+
+    window.location = url;
 }
