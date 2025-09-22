c779e9570d0d85212ecf891c5d5e17d6476cc85c
B4nanaJuice <57623972+B4nanaJuice@users.noreply.github.com>
2025-09-22 16:18:30 +0200

    Added button to add a tag (mobile support)

--- static/inputTagsManager.js
+++ static/inputTagsManager.js
@@ -1,13 +1,23 @@
 inputTags = document.querySelector(".input-tags");
+
 inputTags.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        event.preventDefault();
-        tagName = inputTags.value;
-        addTag(tagName);
+        triggerNewTag();
     }
 })
 
+function triggerNewTag() {
+    newTagName = inputTags.value;
+    addTag(newTagName);
+}
+
+function isEmptyOrSpaces(str) {
+    return str === null || str.match(/^ *$/) !== null;
+}
+
 function addTag(tagName) {
+    if (isEmptyOrSpaces(tagName)) return;
+
     newTag = document.createElement('span');
     newTag.innerHTML = `
         <p>${tagName}</p>
