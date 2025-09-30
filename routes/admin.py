e9ef7a3ab2fd707003bf84389b035d9135189bfb
B4nanaJuice <griesmaxime2@gmail.com>
2025-09-30 13:49:50 +0200

    Set admin panel as jinja template

--- routes/admin.py
+++ routes/admin.py
@@ -28,7 +28,7 @@ def admin_panel():
     ingredient_stock: List[str] = list(db.session.execute(db.select(IngredientStock.name)).scalars())
     ingredients: List[str] = list(set(db.session.execute(db.select(CocktailIngredient.name)).scalars()))
 
-    return render_template("admin.html", beers = beers, ingredient_stock = ingredient_stock, ingredients = ingredients, page_title = "Panel admin")
+    return render_template("admin.html.jinja", beers = beers, ingredient_stock = ingredient_stock, ingredients = ingredients, page_title = "Panel admin")
 
 @page.route("/add-beer", methods = ["POST"])
 def add_beer():
