d4be1b0fcb61ca7edd0aaba77ce93cf577e6ca64
B4nanaJuice <57623972+B4nanaJuice@users.noreply.github.com>
2025-11-09 16:35:15 +0100

    Added base functions for modal management

--- static/modalHelper.js
+++ static/modalHelper.js
@@ -1,5 +1,22 @@
-function createModal(header, content, footer) {
-    console.log("Create a modal")
+let modal = document.querySelector(".modal");
+let modalContent = document.querySelector(".modal-content");
+
+let modalHeader = document.querySelector(".modal-header");
+let modalBody = document.querySelector(".modal-body");
+let modalFooter = document.querySelector(".modal-footer");
+
+function openModal() {
+    modal.classList.add("open");
 }
 
+function closeModal() {
+    modal.classList.remove("open")
+}
 
+function createModal(title, body, footer) {
+    modalHeader.innerText = title;
+    modalBody.innerText = body;
+    modalFooter.innerText = footer;
+
+    openModal();
+}
