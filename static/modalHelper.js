708d908c9a6b6523b00d16dc49ca6b560f82614b
B4nanaJuice <griesmaxime2@gmail.com>
2025-11-10 22:45:56 +0100

    Added functions to generate cocktail order

--- static/modalHelper.js
+++ static/modalHelper.js
@@ -1,9 +1,9 @@
 let modal = document.querySelector(".modal");
 let modalContent = document.querySelector(".modal-content");
 
-let modalHeader = document.querySelector(".modal-header");
-let modalBody = document.querySelector(".modal-body");
-let modalFooter = document.querySelector(".modal-footer");
+modalHeader = document.querySelector(".modal-header");
+modalBody = document.querySelector(".modal-body");
+modalFooter = document.querySelector(".modal-footer");
 
 function openModal() {
     modal.classList.add("open");
@@ -16,7 +16,36 @@ function closeModal() {
 function createModal(title, body, footer) {
     modalHeader.innerText = title;
     modalBody.innerText = body;
+    document.querySelector(".modal-body").innerHTML = body;
     modalFooter.innerText = footer;
 
     openModal();
+}
+
+function createCocktailOrderModal(cocktailId, cocktailName, cocktailIngredients) {
+    modalBody = `
+        <div class="flex flex-column">
+            <p>Ingrédients : ${cocktailIngredients.join(' - ')}</p>
+            <div class="flex flex-row justify-around">
+                <input type="text" name="name" id="name" class="col col-4" placeholder="Entre ton nom" required>
+                <button onclick="orderCocktail(${cocktailId}, this.parentElement.querySelector('input').value)" class="button-outline button-primary col col-4">Commander</button>
+            </div>
+        </div>    
+    `
+
+    createModal(cocktailName, modalBody, "");
+}
+
+async function orderCocktail(cocktailId, name) {
+    orderUrl = `${window.location.origin}/api/order-cocktail?cocktail-id=${cocktailId}&client-name=${name}`;
+    try {
+        let response = await fetch(orderUrl);
+        if (!response.ok) {
+            console.log("error hmmmm");
+        }
+
+        console.log(response.json());
+    } catch (error) {
+        console.log(error)
+    }
 }
