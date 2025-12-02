9566168fbcad9fc4c9dff15596948b68f68b7b32
B4nanaJuice <57623972+B4nanaJuice@users.noreply.github.com>
2025-12-02 14:52:03 +0100

    Simple login/logout

--- routes/auth.py
+++ routes/auth.py
@@ -1,40 +1,36 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, request, redirect, url_for, current_app, session, flash, render_template
 import os
 
-page = Blueprint("auth", __name__, template_folder = "templates", static_folder = "static", url_prefix = "/auth")
+page = Blueprint(name = 'auth', import_name = __name__, url_prefix = '/auth', template_folder = 'templates', static_folder = 'static')
 
-@page.route("/login", methods = ["GET", "POST"])
+@page.route('/login', methods = ['GET', 'POST'])
 def login():
-    return_url: str = request.args.get("return_url") or None
+    
+    if request.method == 'POST':
 
-    if 'user' in session and session['user'] == os.getenv("ADMIN_USERNAME"):
-        if return_url:
-            return redirect(return_url)
+        current_app.logger.info('Someone is trying to log in.')
+        username: str = request.form.get('username')
+        password: str = request.form.get('password')
+
+        if username != os.getenv('ADMIN_USERNAME') or password != os.getenv('ADMIN_PASSWORD'):
+            current_app.logger.warning('Someone entered wrong credientials.')
+            flash('Wrong credientials', 'error')
+            return render_template('auth/login.html')
+        
+        session['auth'] = username
+        current_app.logger.info('Someone logged in.')
+
+        if 'redirect' in session:
+            return redirect(session.pop('redirect'))
         return redirect(url_for('admin.admin_panel'))
     
-    if request.method == "POST":
-        # Get login information
-        username: str = request.form.get("username") or None
-        password: str = request.form.get("password") or None
+    return render_template('auth/login.html')
 
-        # Check login information
-        if not username or not password:
-            return redirect(url_for('public.cocktails'))
-        
-        if username != os.getenv("ADMIN_USERNAME") or password != os.getenv("ADMIN_PASSWORD"):
-            return redirect(url_for('public.cocktails'))
-        
-        session["user"] = os.getenv("ADMIN_USERNAME")
-
-        if return_url:
-            return redirect(return_url)
-        return redirect(url_for('admin.admin_panel'))
-
-    return render_template("login.html.jinja", page_title = "Login")
-
-@page.route("/logout", methods = ["GET"])
+@page.route('/logout', methods = ['GET'])
 def logout():
-    if 'user' in session:
-        session.pop('user')
 
-    return redirect(url_for('public.cocktails'))
+    if 'auth' in session:
+        session.pop('auth')
+        current_app.logger.info('Someone logged out.')
+
+    return redirect(url_for('public.home'))
